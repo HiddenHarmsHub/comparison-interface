49a8783658ab6ca3c3dd7d07696e5a18bd35335d
Cat Smith <910384+catsmith@users.noreply.github.com>
2025-03-19 20:19:20 +0000

    add an option for full html embedding (saved from word)

--- comparison_interface/views/policies.py
+++ comparison_interface/views/policies.py
@@ -21,16 +21,21 @@ class Policies(Request):
                     'site_policies_back_button': WS.get_text(WS.SITE_POLICIES_BACK_BUTTON_LABEL, self._app),
                 },
             )
-        # otherwise we are have html to inject instead
+        # otherwise we are have html to inject instead which could either be a fragment of a full page
         site_policies_html = WS.get_behaviour_conf(WS.BEHAVIOUR_SITE_POLICIES_HTML, self._app)
         with open(os.path.join(self._app.root_path, site_policies_html)) as input_file:
-            html_fragment = input_file.read()
+            html = input_file.read()
 
+        if html.startswith('<html'):
+            fragment = False
+        else:
+            fragment = True
         return self._render_template(
             'pages/policies.html',
             {
                 'google_doc': False,
-                'html_string': html_fragment,
+                'fragment': fragment,
+                'html_string': html,
                 'site_policies_back_button': WS.get_text(WS.SITE_POLICIES_BACK_BUTTON_LABEL, self._app),
             },
         )
