34168cbd7c64b9221c48939f2f9823d75629a944
Cat Smith <910384+catsmith@users.noreply.github.com>
2025-06-18 14:39:41 +0100

    add another output table generated from existing data

--- comparison_interface/db/export.py
+++ comparison_interface/db/export.py
@@ -16,6 +16,31 @@ class Exporter:
         self.app = app
         self.models = [Group, Item, ItemGroup, UserGroup, Comparison, CustomItemPair, UserItem]
 
+    def get_outcome_data(self):
+        """Get the data for the outcome table in the form of a list of dictionaries.
+
+        If the comparison was skipped it is ignored.
+        If item 1 was selected then the outcome is 0.
+        If item 2 was selected then the outcome is 1.
+        If the comparison was tied then the outcome is 2.
+        """
+        data_list = []
+        data = Comparison.query.all()
+        for item in data:
+            if item.state != 'skipped':
+                entry = {
+                    'item_1_id': item.item_1_id,
+                    'item_2_id': item.item_2_id,
+                }
+                if item.state == 'tied':
+                    entry['outcome'] = 2
+                elif item.selected_item_id == item.item_1_id:
+                    entry['outcome'] = 0
+                elif item.selected_item_id == item.item_2_id:
+                    entry['outcome'] = 1
+                data_list.append(entry)
+        return data_list
+
     def save(self, location, file_type):
         """Export the database tables to a zip file of either csv or tsv files."""
         output_directory = os.path.join(location, 'database_tables')
@@ -60,6 +85,16 @@ class Exporter:
             writer.writerow(columns)
             for record in results:
                 writer.writerow(record)
+
+        # make a new table from the data used for BSBT analysis
+        table = Table('outcome', db_meta)
+        data_list = self.get_outcome_data()
+        if len(data_list) > 0:
+            keys = data_list[0].keys()
+            with open(os.path.join(output_directory, f'outcome.{file_type}'), mode='w', encoding='utf-8') as csv_out:
+                csv_writer = csv.DictWriter(csv_out, keys, delimiter=delimiter)
+                csv_writer.writeheader()
+                csv_writer.writerows(data_list)
 
         # zip the folder
         shutil.make_archive(zip_path, 'zip', location)
