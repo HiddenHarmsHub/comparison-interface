428f0a3493fcc7280d794591137bf3d816af4104
Cat Smith <910384+catsmith@users.noreply.github.com>
2025-06-18 16:00:02 +0100

    add a test for the new outcome api table

--- tests_python/integration_tests/test_api.py
+++ tests_python/integration_tests/test_api.py
@@ -145,9 +145,63 @@ def test_items_api_available_when_switched_on_if_key_sent(equal_weight_client_ap
     """
     GIVEN a flask app configured for testing, with equal weights, API_ACCESS and a key file
     WHEN the api items url is requested and the api key is provided
-    THEN a 200 code is received and the comparison data is returned
+    THEN a 200 code is received and the item data is returned
     """
     response = equal_weight_client_api.get("/api/items", headers={'x-api-key': 'test-key'})
     assert response.status_code == 200
     assert b'item_id,name,display_name,image_path,created_date' in response.data
     assert b'1,north_east,North East,item_1.png' in response.data
+
+
+@pytest.mark.usefixtures('key_file')
+def test_outcomes_api_available_and_correct_when_switched_on_if_key_sent(equal_weight_client_api):
+    """
+    GIVEN a flask app configured for testing, with equal weights, API_ACCESS and a key file
+    WHEN the api outcomes url is requested and the api key is provided
+    THEN a 200 code is received and the correct outcome data is returned
+    """
+    comparison_data_list = [
+        {
+            'user_id': 1,
+            'item_1_id': 1,
+            'item_2_id': 2,
+            'selected_item_id': 1,
+            'state': 'selected',
+            'created': datetime.now(timezone.utc),
+            'updated': datetime.now(timezone.utc),
+        },
+        {
+            'user_id': 1,
+            'item_1_id': 4,
+            'item_2_id': 3,
+            'selected_item_id': None,
+            'state': 'skipped',
+            'created': datetime.now(timezone.utc),
+            'updated': datetime.now(timezone.utc),
+        },
+        {
+            'user_id': 1,
+            'item_1_id': 3,
+            'item_2_id': 4,
+            'selected_item_id': 4,
+            'state': 'selected',
+            'created': datetime.now(timezone.utc),
+            'updated': datetime.now(timezone.utc),
+        },
+        {
+            'user_id': 1,
+            'item_1_id': 1,
+            'item_2_id': 3,
+            'selected_item_id': None,
+            'state': 'tied',
+            'created': datetime.now(timezone.utc),
+            'updated': datetime.now(timezone.utc),
+        },
+    ]
+    for comparison_data in comparison_data_list:
+        comparison = Comparison(**comparison_data)
+        db.session.add(comparison)
+    db.session.commit()
+    response = equal_weight_client_api.get("/api/outcomes", headers={'x-api-key': 'test-key'})
+    assert response.status_code == 200
+    assert response.data == b'item_1_id,item_2_id,outcome\r\n1,2,0\r\n3,4,1\r\n1,3,2\r\n'
